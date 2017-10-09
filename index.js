9a779698286883b0a6049d68d1336602555772d0
deebnntt <daniellebennettchan@gmail.com>
2017-10-09 18:35:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  return drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location) {
+  return drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  const driversCopy = [...drivers];
+  return driversCopy.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(drivers) {
+  const driversCopy = [...drivers];
+  return driversCopy.sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drivers) {
+  let totalRevnue = 0;
+
+  drivers.forEach(function(driver) {
+    totalRevnue += driver.revenue;
+  });
+
+  return totalRevnue;
+}
+
+function averageRevenue(drivers) {
+  let totalRevnue = 0;
+
+  drivers.forEach(function(driver) {
+    totalRevnue += driver.revenue;
+  });
+
+  return totalRevnue / drivers.length;
+}
+
+
+// const revs = [];
+//
+// drivers.forEach(function(driver) {
+//   revs.push(driver.revenue);
+// });
+//
+// revs
